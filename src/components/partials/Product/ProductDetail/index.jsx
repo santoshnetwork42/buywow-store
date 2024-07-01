e6248bbc3f482165780c3d0c6880c721f2a95740
piyushjain <piyush@devxconsultancy.com>
2024-07-01 12:17:22 +0530

    in-progress

--- src/components/partials/Product/ProductDetail/index.jsx
+++ src/components/partials/Product/ProductDetail/index.jsx
@@ -1,5 +1,6 @@
 "use client"; // added this just for static data as of now - "productData"
 
+import { StarIcon } from "@/assets/svg/icons";
 import { Text } from "@/components/common";
 import { productData } from "@/data/productData";
 import React from "react";
@@ -8,25 +9,76 @@ const ProductDetail = () => {
   const productDetail = productData[0];
   console.log("productDetail :>> ", productDetail);
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+    <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
       <div className="order-2 bg-red-500 md:order-1 md:row-span-2">
         <p>First Column Content</p>
       </div>
 
       <div className="order-1 md:order-2">
-        <div className="flex flex-col">
-          <Text className="line-clamp-2 font-medium" as="p" size="text2xl">
+        <div className="flex flex-col gap-1">
+          <Text
+            className="line-clamp-2 font-medium"
+            as="p"
+            size="2xl"
+            responsive={false}
+          >
             {productDetail.title}
           </Text>
 
-          <Text as="p" className="line-clamp-3 w-full !font-light" size="texts">
+          <Text
+            as="p"
+            className="line-clamp-3 font-light"
+            size="sm"
+            responsive={false}
+          >
             {productDetail?.benefits}
           </Text>
+          <div className="flex items-center gap-1">
+            <StarIcon size={14} />
+            <Text className="font-light" size="sm" responsive={false}>
+              {productDetail.rating} ({productDetail.reviewCount} Reviews)
+            </Text>
+          </div>
         </div>
       </div>
 
-      <div className="order-3 bg-green-500 md:order-3">
-        <p>Second Column, Second Row Content</p>
+      <div className="order-3">
+        <div className="flex items-center gap-2">
+          <Text as="p" size="xl" className="font-medium" responsive={false}>
+            {productDetail.pricing.current}
+          </Text>
+          {productDetail.pricing.original && (
+            <div className="flex items-center gap-1">
+              <Text
+                as="p"
+                size="md"
+                className="font-light capitalize"
+                responsive={false}
+              >
+                MRP:
+              </Text>
+              <Text
+                as="p"
+                size="md"
+                className="line-through"
+                responsive={false}
+              >
+                {productDetail.pricing.original}
+              </Text>
+            </div>
+          )}
+          {productDetail.pricing.discount && (
+            <div className="flex items-center gap-1">
+              <Text as="p" size="md" responsive={false}>
+                {productDetail.pricing.discount}% Off
+              </Text>
+            </div>
+          )}
+          <Text as="p" size="sm" responsive={false} className="font-light">
+            MRP incl. of all taxes
+          </Text>
+        </div>
+        <div></div>
       </div>
     </div>
   );
