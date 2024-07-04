cc6a848a2e2068b0611758f7491aab87a6d553a1
piyushjain <piyush@devxconsultancy.com>
2024-07-04 15:48:26 +0530

    product images draft2

--- src/components/partials/Product/ProductDetails/ProductImageSection/index.jsx
+++ src/components/partials/Product/ProductDetails/ProductImageSection/index.jsx
@@ -1,34 +1,62 @@
 import { Img } from "@/components/common";
 import { useDeviceWidth } from "@/hooks/useDeviceWidth";
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const ProductImageSection = ({ imageList }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const thumbnailRefs = useRef([]);
+  useEffect(() => {
+    if (thumbnailRefs.current[currentIndex]) {
+      thumbnailRefs.current[currentIndex].scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "start",
+      });
+    }
+  }, [currentIndex]);
+
   const width = useDeviceWidth();
   if (!width) return;
 
-  const items = imageList.map((data, index) => {
-    const isDesktop = width > 576;
-    const dimensions = isDesktop
-      ? { width: 620, height: 480 }
-      : { width: 351, height: 303 };
-    const aspectRatio = `${dimensions.width} / ${dimensions.height}`;
+  const isDesktop = width > 576;
+  const dimensions = isDesktop
+    ? { width: 620, height: 480 }
+    : { width: 351, height: 303 };
+  const aspectRatio = `${dimensions.width} / ${dimensions.height}`;
 
-    return (
-      <React.Fragment key={index}>
-        <Img
-          src={data}
-          width={dimensions.width}
-          height={dimensions.height}
-          alt={`hero image ${index}`}
-          className={`w-full object-contain`}
-          style={{ aspectRatio }}
-        />
-      </React.Fragment>
-    );
-  });
+  const handleDotClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  const items = imageList.map((data, index) => (
+    <div
+      key={index}
+      className={`w-full flex-shrink-0 transform transition-transform duration-500 ${
+        currentIndex === index ? "translate-x-0" : `translate-x-full`
+      }`}
+      style={{ display: currentIndex === index ? "block" : "none" }}
+    >
+      <Img
+        src={data}
+        width={dimensions.width}
+        height={dimensions.height}
+        alt={`hero image ${index}`}
+        className="w-full object-contain"
+        style={{ aspectRatio }}
+      />
+    </div>
+  ));
 
   const thumbnailItems = imageList.map((data, index) => (
-    <div key={index} className="cursor-pointer">
+    <div
+      key={index}
+      className={`cursor-pointer border p-1 ${
+        currentIndex === index ? "border-blue-500" : "border-transparent"
+      }`}
+      ref={(el) => (thumbnailRefs.current[index] = el)}
+      onClick={() => handleDotClick(index)}
+    >
       <Img
         src={data}
         width={351}
@@ -41,11 +69,24 @@ const ProductImageSection = ({ imageList }) => {
   ));
 
   return (
-    <div className="flex max-h-[34rem] justify-center">
-      <div className="hidden flex-col gap-2 overflow-scroll py-2 sm:flex sm:w-[20%]">
+    <div className="flex max-h-[34rem] items-center justify-center">
+      <div className="hidden max-h-[34rem] flex-col gap-2 overflow-scroll py-2 sm:flex sm:w-[25%] md:w-[20%]">
         {thumbnailItems}
       </div>
-      <div className="flex gap-2 overflow-scroll py-2 sm:w-[80%]">{items}</div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex w-full gap-2 overflow-hidden py-2">{items}</div>
+        <div className="mt-4 flex space-x-2">
+          {imageList.map((_, index) => (
+            <div
+              key={index}
+              onClick={() => handleDotClick(index)}
+              className={`h-2 w-2 cursor-pointer rounded-full ${
+                currentIndex === index ? "bg-gray-800" : "bg-gray-400"
+              }`}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
