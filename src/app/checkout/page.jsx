7a5e3242283204de076c63a9104cbd44eeb99882
piyushjain <piyush@devxconsultancy.com>
2024-08-12 15:51:05 +0530

    manage spacing on checkout page

--- src/app/checkout/page.jsx
+++ src/app/checkout/page.jsx
@@ -4,8 +4,12 @@ import OrderSection from "@/components/partials/Checkout/OrderSection";
 export default function Checkout() {
   return (
     <div className="container-main mb-8 flex w-full flex-col justify-center gap-8 py-4 sm:gap-10 md:flex-row lg:gap-12">
-      <Address />
-      <OrderSection />
+      <div className="md:w-6/12">
+        <Address />
+      </div>
+      <div>
+        <OrderSection />
+      </div>
     </div>
   );
 }
