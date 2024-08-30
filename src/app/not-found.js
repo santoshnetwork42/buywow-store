20bb7556d9303676def3a6c6dcbbaae0a8888553
jaimin-devx <jaimin.malaviya@devxconsultancy.com>
2024-08-30 21:39:20 +0530

    WIP on prod: 8fef63d WIP on prod: 44cb7fa Merge branch 'prod' of https://github.com/bodycupid/wow-frontend-v3 into prod

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,21 +1,21 @@
-import { Text } from "@/components/elements";
+import { Heading, Text } from "@/components/elements";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="container-main flex max-h-screen min-h-96 flex-1 flex-col items-center justify-center">
-      <h1 className="mb-4 text-6xl font-bold text-gray-800">404</h1>
-      <h2 className="mb-6 text-2xl font-semibold text-gray-600">
+      <Heading className="mb-4 text-6xl font-bold text-gray-800">404</Heading>
+      <Heading className="mb-6 text-2xl font-semibold text-gray-600">
         Page Not Found
-      </h2>
-      <p className="mb-8 max-w-md text-center text-gray-500">
+      </Heading>
+      <Text className="mb-8 max-w-md text-center text-gray-500">
         {`Oops! The page you're looking for doesn't exist or has been moved.`}
-      </p>
+      </Text>
       <Link
         href="/"
-        className="text-white rounded-full bg-yellow-900 px-6 py-3 transition duration-300"
+        className="rounded-full bg-yellow-900 px-6 py-3 transition duration-300"
       >
-        <Text className="text-white-a700_01">Go Back Home</Text>
+        <Text className="font-medium text-white-a700_01">Go Back Home</Text>
       </Link>
     </div>
   );
--- src/utils/handleRedirect.js
+++ src/utils/handleRedirect.js
@@ -7,23 +7,30 @@ import { notFound, redirect } from "next/navigation";
 import { extname } from "path";
 
 const handleRedirect = async (path) => {
+  console.log("path", path);
   const extension = extname(path);
 
   const pageRedirect = await getRedirectsAPI(path);
 
+  console.log("pageRedirect", pageRedirect);
   if (pageRedirect?.redirect && pageRedirect.redirect !== path) {
+    console.log("redirecting to", pageRedirect.redirect);
     redirect(pageRedirect.redirect.replace("\r", ""));
   }
 
-  if (!!pageRedirect?.slug) {
+  if (pageRedirect?.slug) {
+    console.log("slug found updating redirect");
     await updateRedirectsAPI(path, (pageRedirect?.hitCount || 0) + 1);
   }
 
   if (!pageRedirect && !extension) {
-    await createRedirectsAPI(path);
+    console.log("no page redirect found creating new");
+    const response = await createRedirectsAPI(path);
+    console.log("response", response);
   }
 
-  notFound();
+  console.log("not found");
+  return notFound();
 };
 
 export default handleRedirect;
