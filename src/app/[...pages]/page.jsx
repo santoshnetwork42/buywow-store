25b77f2b0dd2bdc6ab3736485f0125c6d507d30f
jaimin-devx <jaimin.malaviya@devxconsultancy.com>
2024-09-05 15:50:56 +0530

    fixes

--- src/app/[...pages]/page.jsx
+++ src/app/[...pages]/page.jsx
@@ -125,7 +125,7 @@ const componentMap = {
   ComponentBlocksRecentlyViewed: RecentlyViewed,
 };
 
-// export const revalidate = 60;
+export const fetchCache = "only-cache";
 
 const pageType = {
   HOME: "",
