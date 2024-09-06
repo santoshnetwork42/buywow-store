47ebf2b482e1c3ee9e0f80e2883e83c3361751df
krishna-devx <krishna@devxconsultancy.com>
2024-09-06 08:02:58 +0530

    service worker logs

--- src/components/scripts/ServiceWorkerUnregistration.jsx
+++ src/components/scripts/ServiceWorkerUnregistration.jsx
@@ -5,8 +5,10 @@ import { useEffect } from "react";
 export function ServiceWorkerUnregistration() {
   useEffect(() => {
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+        console.log("unregistering service worker>>>");
       navigator.serviceWorker.getRegistrations().then(function (registrations) {
         for (let registration of registrations) {
+            console.log("unregistering service worker>>>", registration);
           registration.unregister();
         }
       });
