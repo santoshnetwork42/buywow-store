71d7b8202609bb58ac92f1c3d097cdbf5de88934
krishna-devx <krishna@devxconsultancy.com>
2024-09-06 11:39:05 +0530

    fixes

--- src/components/scripts/ServiceWorkerUnregistration.jsx
+++ src/components/scripts/ServiceWorkerUnregistration.jsx
@@ -9,14 +9,25 @@ export function ServiceWorkerUnregistration() {
       console.log("navigator>>>>", navigator);
       const registrations = navigator.serviceWorker;
       console.log("registrations>>>>", registrations);
-      navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        console.log("registrations>>>>>>>>>", registrations);
-        for (let registration of registrations) {
-          console.log("registration>>>", registration);
-          console.log("unregistering service worker>>>", registration);
-          registration.unregister();
-        }
+    //   navigator.serviceWorker.getRegistrations().then(function (registrations) {
+    //     console.log("registrations>>>>>>>>>", registrations);
+    //     for (let registration of registrations) {
+    //       console.log("registration>>>", registration);
+    //       console.log("unregistering service worker>>>", registration);
+    //       registration.unregister();
+    //     }
+    //   });
+    navigator.serviceWorker.ready.then((registration) => {
+        console.log("Service worker ready:", registration);
+        registration.unregister().then(() => {
+          console.log("Successfully unregistered service worker");
+        }).catch((error) => {
+          console.error("Failed to unregister service worker:", error);
+        });
+      }).catch((error) => {
+        console.error("Service worker not ready:", error);
       });
+      
     }
   }, []);
 
