78cf9a97bc44a29e7d5f85079654c66cb90e9e13
piyushjain <piyush@devxconsultancy.com>
2024-09-12 14:00:37 +0530

    footer toggle arrow animation

--- src/components/features/Accordion/AccordionToggle/index.jsx
+++ src/components/features/Accordion/AccordionToggle/index.jsx
@@ -1,6 +1,12 @@
 import { twMerge } from "tailwind-merge";
 
-const ToggleArrow = ({ open, onToggle, className, variant }) => {
+const ToggleArrow = ({
+  open,
+  onToggle,
+  className,
+  variant,
+  arrowClassName,
+}) => {
   return (
     <div
       className={twMerge(
@@ -15,6 +21,7 @@ const ToggleArrow = ({ open, onToggle, className, variant }) => {
           "w-2.5 origin-center transform rounded-full bg-black-900 transition-transform duration-300 ease-in-out",
           variant === "small" ? "h-[1px] -translate-x-[1px]" : "h-0.5",
           open ? "-rotate-45" : "rotate-45",
+          arrowClassName,
         )}
       ></div>
       <div
@@ -22,6 +29,7 @@ const ToggleArrow = ({ open, onToggle, className, variant }) => {
           "-mx-1 w-2.5 origin-center transform rounded-full bg-black-900 transition-transform duration-300 ease-in-out",
           variant === "small" ? "h-[1px]" : "h-0.5",
           open ? "rotate-45" : "-rotate-45",
+          arrowClassName,
         )}
       ></div>
     </div>
