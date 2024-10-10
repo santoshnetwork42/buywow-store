215879ddee3c473734da3d0f3fad64eabdad22ce
piyushjain <piyush@devxconsultancy.com>
2024-10-10 11:15:27 +0530

    vercel deploy

--- src/utils/context/navbar.js
+++ src/utils/context/navbar.js
@@ -28,7 +28,7 @@ function NavbarProvider({ children, headerData }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const _source = searchParams.get("_source");
-
+ 
   const [initialData, setInitialData] = useState(null);
 
   useEffect(() => {
