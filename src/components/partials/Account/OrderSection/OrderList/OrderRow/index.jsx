50da63d113b3767ffe683894ce0108b918a7c6d6
piyushjain <piyush@devxconsultancy.com>
2024-11-20 10:08:57 +0530

    track order

--- src/components/partials/Account/OrderSection/OrderList/OrderRow/index.jsx
+++ src/components/partials/Account/OrderSection/OrderList/OrderRow/index.jsx
@@ -12,17 +12,37 @@ const OrderRow = React.memo(({ order }) => {
 
   return (
     <Link prefetch={false} href={`/order/${order.id}`}>
-      <div className="grid grid-cols-[30%_70%] gap-2 border-b py-2 hover:bg-slate-100 md:grid-cols-[22%_36%_20%_auto] md:py-3 lg:py-4 xl:py-[14px]">
+      <div className="grid grid-cols-[30%_70%] gap-2 py-2 md:grid-cols-[22%_30%_20%_10%_auto] md:border-b md:py-3 md:hover:bg-slate-100 lg:py-4 xl:py-[14px]">
         <OrderCell label="Order:" value={`#${order.code}`} isLink />
         <OrderCell label="Date:" value={formateDate(order.orderDate)} />
         <OrderCell label="Status:" value={order.status} style={badgeStyle} />
         <OrderCell label="Total:" value={`₹${toDecimal(order.totalAmount)}`} />
+        <Link
+          prefetch={false}
+          href={`https://track.buywow.in/my-order?order_id=${order?.code}`}
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          className="hidden md:block"
+        >
+          <OrderCell label="Track:" isLink valueLabel="Track" />
+        </Link>
       </div>
+      <Link
+        prefetch={false}
+        href={`https://track.buywow.in/my-order?order_id=${order?.code}`}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="grid grid-cols-[30%_70%] gap-2 md:hidden md:grid-cols-[22%_30%_20%_10%_auto] md:py-3 md:hover:bg-slate-100 lg:py-4 xl:py-[14px]"
+      >
+        <OrderCell label="Track:" isLink valueLabel="Track" />
+      </Link>
     </Link>
   );
 });
 
-const OrderCell = React.memo(({ label, value, isLink, style }) => (
+const OrderCell = React.memo(({ label, value, isLink, style, valueLabel }) => (
   <>
     <Heading as="h4" size="base" className="text-sm md:hidden" responsive>
       {label}
@@ -34,7 +54,7 @@ const OrderCell = React.memo(({ label, value, isLink, style }) => (
       style={style}
       responsive
     >
-      {value}
+      {!!valueLabel ? valueLabel : value}
     </Text>
   </>
 ));
