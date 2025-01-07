73d8b46b0a7b4204ff81bd700ddcf4e16936942d
piyushjain <piyush@devxconsultancy.com>
2025-01-07 16:50:46 +0530

    add category timer

---
+++ src/components/blocks/FeaturedCategories/FlipClock/index.jsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { Text } from "@/components/elements";
+import { useCallback, useEffect, useState } from "react";
+import styles from "@/src/components/partials/Others/FlipClock/flipClock.module.css";
+import ClockIcon from "@/assets/svg/clockIcon";
+
+const FlipUnit = ({ digit, unit }) => {
+  const [displayedDigit, setDisplayedDigit] = useState(digit);
+
+  useEffect(() => {
+    const displayTimer = setTimeout(() => setDisplayedDigit(digit), 300);
+
+    return () => {
+      clearTimeout(displayTimer);
+    };
+  }, [digit]);
+
+  return (
+    <div className="flex h-6 w-4 flex-col overflow-hidden rounded-sm">
+      <div className="relative mt-0.5 flex-1">
+        <div
+          className={`absolute top-0 z-10 flex h-1/2 w-full items-end justify-center overflow-hidden`}
+        >
+          <span className="-mb-2.5 text-sm text-white-a700_01">
+            {displayedDigit}
+          </span>
+        </div>
+        <div className="absolute bottom-0 flex h-1/2 w-full items-start justify-center overflow-hidden">
+          <span className="-mt-2.5 text-sm text-white-a700_01">
+            {displayedDigit}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+FlipUnit.displayName = "FlipUnit";
+
+const FlipClock = ({ timer, centerText }) => {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const calculateTimeLeft = useCallback(() => {
+    const now = new Date();
+    const startDateTime = new Date(`${timer.startDate}T${timer.startTime}`);
+    const endDateTime = new Date(`${timer.endDate}T${timer.endTime}`);
+
+    if (
+      now < startDateTime ||
+      now > endDateTime ||
+      startDateTime > endDateTime
+    ) {
+      return null;
+    }
+
+    let targetDate = endDateTime;
+    if (timer.type === "DAILY") {
+      // For daily timer, set target to today's end time
+      targetDate = new Date(now);
+      if (!!timer.endTime) {
+        const [endHours, endMinutes, endSeconds] = timer.endTime
+          .split(":")
+          .map(Number);
+        targetDate.setHours(endHours, endMinutes, endSeconds, 0);
+
+        if (targetDate <= now) {
+          return null;
+        }
+      }
+    }
+
+    const distance = targetDate.getTime() - now.getTime();
+
+    const formatWithLeadingZero = (value) => {
+      return value < 10 ? `0${value}` : value.toString();
+    };
+
+    return {
+      days: formatWithLeadingZero(Math.floor(distance / (1000 * 60 * 60 * 24))),
+      hours: formatWithLeadingZero(
+        Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      ),
+      minutes: formatWithLeadingZero(
+        Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+      ),
+      seconds: formatWithLeadingZero(
+        Math.floor((distance % (1000 * 60)) / 1000),
+      ),
+    };
+  }, [timer]);
+
+  useEffect(() => {
+    const updateTimer = () => {
+      const newTimeLeft = calculateTimeLeft();
+      setTimeLeft(newTimeLeft);
+    };
+
+    updateTimer(); // Initial call
+    const interval = setInterval(updateTimer, 1000);
+
+    return () => clearInterval(interval);
+  }, [calculateTimeLeft]);
+
+  if (!timer || !timer.startDate || !timer.endDate) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      {centerText && (
+        <div className="flex items-center">
+          <Text as="p" className="text-white-a700_01" size="sm" responsive>
+            {centerText}
+          </Text>
+        </div>
+      )}
+      {!!timeLeft && (
+        <div className="flex items-center justify-center gap-1 px-1">
+          <div className="flex items-center">
+            <FlipUnit digit={timeLeft.minutes} unit="MIN" />
+            <Text className="mb-1 text-white-a700_01">:</Text>
+            <FlipUnit digit={timeLeft.seconds} unit="SEC" />
+          </div>
+          <ClockIcon size={14} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+FlipClock.displayName = "FlipClock";
+
+export default FlipClock;
--- src/components/blocks/FeaturedCategories/index.jsx
+++ src/components/blocks/FeaturedCategories/index.jsx
@@ -1,12 +1,103 @@
+"use client";
 import LinkClickTracker from "@/components/common/LinkClickTracker";
 import SectionHeading from "@/components/common/SectionHeading";
-import { Heading, Img, Text } from "@/components/elements";
+import { Img, Text } from "@/components/elements";
 import Slider from "@/components/features/Slider";
+import CategoryFlipClock from "./FlipClock";
+import FlipClock from "@/components/partials/Others/FlipClock";
 import { extractAttributes } from "@/utils/helpers";
+import { useEffect, useState } from "react";
+
+const CustomCountdown = ({ startTime }) => {
+  const [timeLeft, setTimeLeft] = useState({
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const now = new Date();
+      const [startHours = "", startMinutes = ""] = startTime
+        ?.split(":")
+        .map(Number);
+
+      const targetTime = new Date(now);
+      targetTime.setHours(startHours, startMinutes, 0);
+
+      // If target time is in the past for today, no countdown needed
+      if (targetTime <= now) return { hours: 0, minutes: 0, seconds: 0 };
+
+      const difference = targetTime - now;
+
+      return {
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    };
+
+    // Initial calculation
+    setTimeLeft(calculateTimeLeft());
+
+    // Update every second
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [startTime]);
+
+  const formatNumber = (num) => String(num).padStart(2, "0");
+
+  return (
+    <div className="flex items-center justify-center gap-1 p-1 sm:gap-2">
+      <div className="flex flex-col items-center">
+        <Text responsive className="rounded bg-white-a700_01 p-1 sm:p-2">
+          {formatNumber(timeLeft.hours)}
+        </Text>
+        <Text responsive className="mt-1 text-white-a700_01">
+          HRS
+        </Text>
+      </div>
+      <Text responsive className="mt-[-1rem] text-white-a700_01">
+        :
+      </Text>
+      <div className="flex flex-col items-center">
+        <Text responsive className="rounded bg-white-a700_01 p-1 sm:p-2">
+          {formatNumber(timeLeft.minutes)}
+        </Text>
+        <Text responsive className="mt-1 text-white-a700_01">
+          MIN
+        </Text>
+      </div>
+      <Text responsive className="mt-[-1rem] font-bold text-white-a700_01">
+        :
+      </Text>
+      <div className="flex flex-col items-center">
+        <Text responsive className="rounded bg-white-a700_01 p-1 sm:p-2">
+          {formatNumber(timeLeft.seconds)}
+        </Text>
+        <Text responsive className="mt-1 text-white-a700_01">
+          SEC
+        </Text>
+      </div>
+    </div>
+  );
+};
 
 const CategoryItem = ({ category, size, parentCategoryTitle, priority }) => {
-  const { image, slug, title } = category;
+  const { image, slug, title, endTime, startDate, endDate, startTime } =
+    category;
+  console.log(
+    "endTime, startDate, endDate, startTime :>> ",
+    endTime,
+    startDate,
+    endDate,
+    startTime,
+  );
   const { url, alternativeText } = extractAttributes(image);
+  const [showClock, setShowClock] = useState(true);
 
   const imageSize = size === "SMALL" ? 260 : 396;
   const imageHeight = size === "SMALL" ? 260 : 470;
@@ -17,11 +108,208 @@ const CategoryItem = ({ category, size, parentCategoryTitle, priority }) => {
       ? "w-[calc(33vw-20px)] min-w-[100px] max-w-[260px] sm:w-[26vw] md:w-[24vw] lg:w-[22vw] xl:w-[20vw]"
       : "w-[calc(33vw-12px)] min-w-[112px] sm:w-[28vw] sm:max-w-[396px] md:w-[26vw] lg:w-[24vw] xl:w-[22vw]";
 
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const currentTime = now.toLocaleTimeString("en-US", { hour12: false });
+    const currentDate = now.toISOString().split("T")[0];
+    return { currentTime, currentDate };
+  };
+
+  const isWithinThreeHoursOfStart = () => {
+    const now = new Date();
+    const currentDate = now.toISOString().split("T")[0];
+
+    // Check if current date is within the valid date range
+    if (!(currentDate >= startDate && currentDate <= endDate)) {
+      return false;
+    }
+
+    // Parse start time
+    const [startHours = "", startMinutes = ""] = startTime
+      ?.split(":")
+      .map(Number);
+
+    // Create date objects for comparison
+    const startTimeDate = new Date(now);
+    startTimeDate.setHours(startHours, startMinutes, 0);
+
+    const threeHoursBefore = new Date(startTimeDate);
+    threeHoursBefore.setHours(startTimeDate.getHours() - 3);
+
+    // Check if current time is within 3 hours before start time
+    return now >= threeHoursBefore && now < startTimeDate;
+  };
+
+  const getSaleStatus = () => {
+    const { currentTime, currentDate } = getCurrentDateTime();
+
+    if (currentDate > endDate) {
+      return "ENDED";
+    }
+
+    if (
+      currentTime >= startTime &&
+      currentTime <= endTime &&
+      currentDate >= startDate &&
+      currentDate <= endDate
+    ) {
+      return "LIVE";
+    }
+
+    if (
+      currentDate >= startDate &&
+      currentDate <= endDate &&
+      isWithinThreeHoursOfStart()
+    ) {
+      return "UPCOMING";
+    }
+
+    return "NORMAL";
+  };
+
+  useEffect(() => {
+    if (getSaleStatus() === "LIVE") {
+      const interval = setInterval(
+        () => {
+          setShowClock((current) => !current);
+          if (!showClock) {
+            setTimeout(() => {
+              setShowClock(true);
+            }, 3000);
+          }
+        },
+        showClock ? 4000 : 3000,
+      );
+
+      return () => clearInterval(interval);
+    }
+  }, [showClock]);
+
+  const LiveIndicator = () => (
+    <div className="flex w-full items-center justify-center gap-1 self-end px-2">
+      <Text size="xs" className="py-0.5 font-medium text-white-a700_01">
+        LIVE
+      </Text>
+      <span className="relative flex h-2 w-2">
+        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white-a700_01 opacity-75"></span>
+        <span className="relative inline-flex h-2 w-2 rounded-full bg-white-a700_01"></span>
+      </span>
+    </div>
+  );
+
+  const renderContent = () => {
+    const status = getSaleStatus();
+
+    switch (status) {
+      case "LIVE":
+        return (
+          <div className="relative">
+            <div
+              className={`overflow-hidden rounded sm:rounded-md lg:rounded-lg ${aspectRatio}`}
+            >
+              <Img
+                src={url}
+                width={imageSize}
+                height={imageHeight}
+                alt={alternativeText || `${slug} Image`}
+                className="h-auto w-full object-contain"
+                priority={priority}
+              />
+            </div>
+            <div className="absolute right-0 top-0.5 mr-0.5 self-end rounded-r-md rounded-bl-md rounded-br-none bg-yellow-900 backdrop-blur-sm backdrop-filter sm:right-1 sm:top-1 sm:mr-0 sm:rounded-r-lg sm:rounded-br-none">
+              <div className="relative">
+                <div
+                  className={`absolute inset-0 transition-all duration-500 ease-in-out ${showClock ? "visible opacity-100" : "invisible opacity-0"}`}
+                >
+                  <CategoryFlipClock
+                    timer={{
+                      type: "DAILY",
+                      startTime,
+                      endTime,
+                      startDate,
+                      endDate,
+                    }}
+                  />
+                </div>
+                <div
+                  className={`absolute inset-0 flex h-full flex-col items-center justify-center transition-all duration-500 ease-in-out ${showClock ? "invisible opacity-0" : "visible opacity-100"}`}
+                >
+                  <LiveIndicator />
+                </div>
+                <div className="invisible">
+                  <CategoryFlipClock
+                    timer={{
+                      type: "DAILY",
+                      startTime,
+                      endTime,
+                      startDate,
+                      endDate,
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+
+      case "UPCOMING":
+        return (
+          <div
+            className="pointer-events-none relative cursor-not-allowed"
+            onClick={(e) => e.preventDefault()}
+          >
+            <div
+              className={`overflow-hidden rounded sm:rounded-md lg:rounded-lg ${aspectRatio}`}
+            >
+              <Img
+                src={url}
+                width={imageSize}
+                height={imageHeight}
+                alt={alternativeText || `${slug} Image`}
+                className="h-auto w-full object-contain"
+                priority={priority}
+              />
+            </div>
+            <div className="absolute left-1/2 top-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-2 rounded-md backdrop-blur-sm">
+              <div className="w-full bg-yellow-50 p-1 text-center">
+                <Text>Sale Starts In</Text>
+              </div>
+              <CustomCountdown startTime={startTime} />
+            </div>
+          </div>
+        );
+
+      case "ENDED":
+      case "NORMAL":
+        return (
+          <div
+            className={`overflow-hidden rounded sm:rounded-md lg:rounded-lg ${aspectRatio}`}
+          >
+            <Img
+              src={url}
+              width={imageSize}
+              height={imageHeight}
+              alt={alternativeText || `${slug} Image`}
+              className="h-auto w-full object-contain"
+              priority={priority}
+            />
+          </div>
+        );
+    }
+  };
+
   return (
     <LinkClickTracker
-      href={`/collections/${slug}` || "#"}
-      className={linkClassName}
+      href={
+        getSaleStatus() === "UPCOMING" ? "#" : `/collections/${slug}` || "#"
+      }
+      className={`${linkClassName} ${getSaleStatus() === "UPCOMING" ? "pointer-events-none cursor-not-allowed" : ""}`}
       trackingType="SHOP_BY_CLICK"
+      onClick={(e) => {
+        if (getSaleStatus() === "UPCOMING") {
+          e.preventDefault();
+        }
+      }}
       trackingEventPayload={{
         slug,
         name: title,
@@ -29,18 +317,7 @@ const CategoryItem = ({ category, size, parentCategoryTitle, priority }) => {
         type: parentCategoryTitle,
       }}
     >
-      <div
-        className={`overflow-hidden rounded sm:rounded-md lg:rounded-lg ${aspectRatio}`}
-      >
-        <Img
-          src={url}
-          width={imageSize}
-          height={imageHeight}
-          alt={alternativeText || `${slug} Image`}
-          className="h-auto w-full object-contain"
-          priority={priority}
-        />
-      </div>
+      {renderContent()}
       <Text
         size="xl"
         as="p"
